d3e553d0960ba8eb17f0bc4923cae5af2824ae1d
Avishaan <avishaan@codeHatcher.com>
2016-04-04 16:38:21 -0500

    spawn enemies at certain positions

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,11 +4,19 @@ using System.Collections;
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
-	
+
     // Use this for initialization
     void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+
+        foreach (Transform child in transform)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+
+            //parent of enemy transform is the enemy formation transform, enemy because child of enemyformation
+            enemy.transform.parent = child;
+        }
+
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Position.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+public class Position : MonoBehaviour
+{
+    void OnDrawGizmos()
+    {
+		Gizmos.DrawWireSphere(transform.position, 1);
+    }
+}
