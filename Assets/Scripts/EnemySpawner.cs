e622e81db893d260aaa541dbcb908355548201ff
Avishaan <avishaan@codeHatcher.com>
2016-04-04 17:22:47 -0500

    fix for when speed is high and enemy edge leaves viewport

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -51,8 +51,11 @@ public class EnemySpawner : MonoBehaviour
 
         float rightEdgeOfFormation = transform.position.x + 0.5f * width;
         float leftEdgeOfFormation = transform.position.x - 0.5f * width;
-        if (leftEdgeOfFormation < xmin || rightEdgeOfFormation > xmax) {
-            movingRight = !movingRight;
+
+        if (leftEdgeOfFormation < xmin) {
+            movingRight = true;
+        } else if (rightEdgeOfFormation > xmax) {
+            movingRight = false;
         }
     }
 }
