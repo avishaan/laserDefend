35d312913cd0affd3701a8c7f8040528552fa47f
Avishaan <avishaan@codeHatcher.com>
2016-04-04 17:14:08 -0500

    enemies change direction when hitting edge

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,10 +4,25 @@ using System.Collections;
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public float width = 10f;
+    public float height = 5f;
+    public float speed = 5f;
+
+    private bool movingRight = true;
+    private float xmax;
+    private float xmin;
 
     // Use this for initialization
     void Start()
     {
+
+        // calculate edge of screen
+        float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftBoundary = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanceToCamera));
+        Vector3 rightBoundary = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distanceToCamera));
+
+        xmax = rightBoundary.x;
+        xmin = leftBoundary.x;
 
         foreach (Transform child in transform)
         {
@@ -19,9 +34,25 @@ public class EnemySpawner : MonoBehaviour
 
     }
 
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height, 0));
+    }
+
     // Update is called once per frame
     void Update()
     {
+        if (movingRight)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        } else {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
 
+        float rightEdgeOfFormation = transform.position.x + 0.5f * width;
+        float leftEdgeOfFormation = transform.position.x - 0.5f * width;
+        if (leftEdgeOfFormation < xmin || rightEdgeOfFormation > xmax) {
+            movingRight = !movingRight;
+        }
     }
 }
