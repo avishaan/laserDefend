548b2f83ac601233ef46e4a04f4196878a27a74e
Avishaan <avishaan@codeHatcher.com>
2016-04-04 18:28:28 -0500

    destroy beams when they go past playable area

---
+++ Assets/Scripts/Shredder.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class Shredder : MonoBehaviour
+{
+
+	void OnTriggerEnter2D(Collider2D col) {
+        Destroy(col.gameObject);
+    }
+
+
+}
